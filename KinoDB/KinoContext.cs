3cee187592a4d20bc3299b440010b7324a144dc9
unknown <l.kruszynski92@wp.pl>
2023-01-09 17:16:46 +0100

    moze zadziala

---
+++ KinoDB/KinoContext.cs
@@ -0,0 +1,25 @@
+﻿using KinoDB.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KinoDB
+{
+    public class KinoContext : DbContext
+    {
+        public DbSet<KinoModel> Films { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptions)
+        {
+            var cs = @"Data Source=(localdb)\local;Initial Catalog=KinoDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+
+
+            dbContextOptions.UseSqlServer(cs);
+        }
+    }
+}
---
+++ KinoDB/Models/KinoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KinoDB.Models
+{
+    public class KinoModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Year { get; set; }
+    }
+}
